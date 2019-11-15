10448c95becf632e2b0b8cf7088269dc7a6b1eb4
Yoshiki Higashi <gigashiy@gmail.com>
2019-11-15 13:37:35 +0900

    [update] 受信のみの場合実装

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Go TCP sample
 
+## Usage
+
+```bash
+make
+./output/app_xxx
+```
+
+```bash
+curl -X POST http://127.0.0.1:8000 \
+-u user:pass \
+-H "Content-Type: application/json" \
+-d '{"test":123}'
+```
--- src/main.go
+++ src/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("listen :8000")
+	listen, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("accept %s\n", conn.RemoteAddr().String())
+
+		buf := make([]byte, 4*1024) // TODO: define buf size
+		for {
+			readCount, err := conn.Read(buf)
+			if err != nil {
+				fmt.Printf("read error:%s\n", err.Error())
+				break
+			}
+			fmt.Println("__________________________________________________")
+			fmt.Println(string(buf[:readCount]))
+		}
+		conn.Close()
+		fmt.Println("__________________________________________________")
+		fmt.Println("disconnect")
+	}
 }
